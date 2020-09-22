abee0f69f317afbe02437afcca59de2f74fe8c9c
cledegario <cassio.leodegario@gmail.com>
2020-09-22 11:31:22 -0400

    refactor: move sut to a factory helper method

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,17 +1,23 @@
 import { InvalidFieldError } from '@/validation/errors';
 import { EmailValidation } from './email-validation';
 import faker from 'faker';
+import { FieldValidation } from '@/validation/protocols/field-validation';
+
+const makeSut = (fieldName: string): FieldValidation => {
+  return new EmailValidation(fieldName);
+};
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const fieldName = faker.random.word();
-    const sut = new EmailValidation(fieldName);
+    const sut = makeSut(fieldName);
     const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError(fieldName));
   });
 
   test('Should return falsy if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word());
+    const fieldName = faker.random.word();
+    const sut = makeSut(fieldName);
     const error = sut.validate(faker.internet.email());
     expect(error).toBeFalsy();
   });
