0a9b961f0669d003e27eb5149e1f3d8bf230692a
cledegario <cassio.leodegario@gmail.com>
2020-09-29 08:23:29 -0400

    feat: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -38,9 +38,9 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
   const handleSubmit = async(event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
     try {
-      // if (state.isLoading || state.emailError || state.passwordError) {
-      //   return;
-      // }
+      if (state.isLoading || state.nameError || state.emailError || state.passwordError || state.passwordConfirmationError) {
+        return;
+      }
       setState({ ...state, isLoading: true });
       await addAccount.add({
         name: state.name,
