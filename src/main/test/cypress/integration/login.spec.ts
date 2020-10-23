5aed2ab116de6dcdb40197359efa9a130c96a3eb
cledegario <cassio.leodegario@gmail.com>
2020-10-23 14:56:02 -0400

    chore: add cypress command

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,15 @@
+
+const montarTestId = (id: string): string => {
+  return `[data-testid="${id}"]`;
+};
+
 describe('Login', () => {
   it('should load with correct initial state', () => {
     cy.visit('login');
+  });
+
+  it('Should load with correct initial state', () => {
+    cy.get(montarTestId('email-status')).should('have.attr', 'title', 'Campo Obrigatório');
+    cy.get(montarTestId('password-status')).should('have.attr', 'title', 'Campo Obrigatório');
   });
 });
