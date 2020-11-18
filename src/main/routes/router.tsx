72431d67cd96353aac853e582bcf60f4622ca4c7
cledegario <cassio.leodegario@gmail.com>
2020-11-18 12:58:51 -0400

    feat: add SurveyList composition

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ApiContext } from '@/presentation/contexts';
-import { SurveyList } from '@/presentation/pages';
 import { makeLogin } from '@/main/factories/pages/login/login-factory';
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory';
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/current-account-adapter';
 import { PrivateRoute } from '@/presentation/components';
+import { makeSurveyList } from '../factories/pages/survey-list/survey-list-factory';
 
 const Router: React.FC = () => {
   return (
@@ -18,7 +18,7 @@ const Router: React.FC = () => {
         <Switch>
           <Route path="/login" exact component={makeLogin} />
           <Route path="/signup" exact component={makeSignUp} />
-          <PrivateRoute path="/" exact component={SurveyList} />
+          <PrivateRoute path="/" exact component={makeSurveyList} />
         </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
