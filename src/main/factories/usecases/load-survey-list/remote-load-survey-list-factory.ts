d27f0692f40e39900d3d5ca6441ae67c1dfbd9fb
cledegario <cassio.leodegario@gmail.com>
2020-11-26 09:07:34 -0400

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,8 @@
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list';
 import { LoadSurveyList } from '@/domain/usecases';
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators';
 import { makeApiUrl } from '../../http/api-url-factory';
-import { makeAxiosHttpClient } from '../../http/axios-http-factory';
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient());
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator());
 };
