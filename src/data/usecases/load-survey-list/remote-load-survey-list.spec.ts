e302dbd4698d44b759a235c716df873c0e8114fa
cledegario <cassio.leodegario@gmail.com>
2020-11-26 09:49:05 -0400

    test: ensure remoteLoadSurveyList map remote data to domain data

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,6 +1,6 @@
 import faker from 'faker';
 import { HttpStatusCode } from '@/data/protocols/http';
-import { HttpGetClientSpy } from '@/data/test';
+import { HttpGetClientSpy, mockRemoteSurveyListModel } from '@/data/test';
 import { UnexpectedError } from '@/domain/errors';
 import { mockSurveyListModel } from '@/domain/test';
 import { RemoteLoadSurveyList } from './remote-load-survey-list';
@@ -57,13 +57,30 @@ describe('RemoteLoadSurveyList', () => {
 
   test('Should return a list of LoadSurveyList.Model if HttpGetClient returns 200', async() => {
     const { sut, httpGetClientSpy } = makeSut();
-    const httpResult = mockSurveyListModel();
+    const httpResult = mockRemoteSurveyListModel();
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.ok,
       body: httpResult
     };
     const surveyList = await sut.loadAll();
-    expect(surveyList).toEqual(httpResult);
+    expect(surveyList).toEqual([{
+      id: httpResult[0].id,
+      question: httpResult[0].question,
+      didAnswer: httpResult[0].didAnswer,
+      date: new Date(httpResult[0].date)
+    },
+    {
+      id: httpResult[1].id,
+      question: httpResult[1].question,
+      didAnswer: httpResult[1].didAnswer,
+      date: new Date(httpResult[1].date)
+    },
+    {
+      id: httpResult[2].id,
+      question: httpResult[2].question,
+      didAnswer: httpResult[2].didAnswer,
+      date: new Date(httpResult[2].date)
+    }]);
   });
 
   test('Should return an empty list if HttpGetClient returns 204', async() => {
