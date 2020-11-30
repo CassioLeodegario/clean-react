2b7caaa476c74fc84218800278d2716706d5d9a8
Cassio Leodegario <cassio.leodegario@outlook.com>
2020-11-30 15:38:24 -0400

    test: ensure RemoteLoadSurveyResult throws UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -42,4 +42,13 @@ describe('RemoteLoadSurveyResult', () => {
     const promise = sut.load();
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('Should thow UnexpectedError if HttpGetClient returns 500', async() => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
