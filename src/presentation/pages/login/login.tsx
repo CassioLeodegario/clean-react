ddb83fe737bf54fa40010b3237d9f93e6570bf3c
Cassio Leodegario <cassio.leodegario@outlook.com>
2020-11-30 09:26:55 -0400

    refactor: change useState to use callback

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,6 +5,7 @@ import { LoginHeader, Input, FormStatus, Footer, SubmitButton } from '@/presenta
 import { FormContext, ApiContext } from '@/presentation/contexts';
 import { Validation } from '@/presentation/protocols/validation';
 import { Authentication } from '@/domain/usecases';
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder';
 
 type Props = {
   validation: Validation,
@@ -23,18 +24,15 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     mainError: ''
   });
 
-  useEffect(() => {
+  useEffect(() => { validate('email'); }, [state.email]);
+  useEffect(() => { validate('password'); }, [state.password]);
+
+  const validate = (field: string): void => {
     const { email, password } = state;
     const formData = { email, password };
-    const emailError = validation.validate('email', formData);
-    const passwordError = validation.validate('password', formData);
-    setState({
-      ...state,
-      emailError,
-      passwordError,
-      isFormInvalid: !!emailError || !!passwordError
-    });
-  }, [state.email, state.password]);
+    setState(old => ({ ...old, [`${field}Error`]: validation.validate(field, formData) }));
+    setState(old => ({ ...old, isFormInvalid: !!old.emailError || !!old.passwordError }));
+  };
 
   const handleSubmit = async(event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -36,22 +36,17 @@ const SignUp: React.FC<Props> = ({
     mainError: ''
   });
 
-  useEffect(() => {
+  useEffect(() => { validate('email'); }, [state.email]);
+  useEffect(() => { validate('password'); }, [state.password]);
+  useEffect(() => { validate('name'); }, [state.name]);
+  useEffect(() => { validate('passwordConfirmation'); }, [state.passwordConfirmation]);
+
+  const validate = (field: string): void => {
     const { name, email, password, passwordConfirmation } = state;
     const formData = { name, email, password, passwordConfirmation };
-    const nameError = validation.validate('name', formData);
-    const emailError = validation.validate('email', formData);
-    const passwordError = validation.validate('password', formData);
-    const passwordConfirmationError = validation.validate('passwordConfirmation', formData);
-    setState({
-      ...state,
-      nameError,
-      emailError,
-      passwordError,
-      passwordConfirmationError,
-      isFormInvalid: !!nameError || !!emailError || !!passwordError || !!passwordConfirmationError
-    });
-  }, [state.name, state.email, state.password, state.passwordConfirmation]);
+    setState(old => ({ ...old, [`${field}Error`]: validation.validate(field, formData) }));
+    setState(old => ({ ...old, isFormInvalid: !!old.emailError || !!old.passwordError || !!old.nameError || !!old.passwordConfirmationError }));
+  };
 
   const handleSubmit = async(
     event: React.FormEvent<HTMLFormElement>
