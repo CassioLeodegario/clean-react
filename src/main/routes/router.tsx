f4e61701c5c8493f67dc195662406d65022bb472
Cassio Leodegario <cassio.leodegario@outlook.com>
2020-12-04 11:41:10 -0400

    refactor: move loading to its own file

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -6,6 +6,7 @@ import { makeSignUp } from '@/main/factories/pages/signup/signup-factory';
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/current-account-adapter';
 import { PrivateRoute } from '@/presentation/components';
 import { makeSurveyList } from '../factories/pages/survey-list/survey-list-factory';
+import { SurveyResult } from '@/presentation/pages';
 
 const Router: React.FC = () => {
   return (
@@ -19,6 +20,7 @@ const Router: React.FC = () => {
           <Route path="/login" exact component={makeLogin} />
           <Route path="/signup" exact component={makeSignUp} />
           <PrivateRoute path="/" exact component={makeSurveyList} />
+          <PrivateRoute path="/surveys" exact component={SurveyResult} />
         </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
