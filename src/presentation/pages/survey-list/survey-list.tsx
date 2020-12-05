9d5eac22415bad8ee3e6b417bef67421cdc578e8
Cassio Leodegario <cassio.leodegario@outlook.com>
2020-12-04 22:42:49 -0400

    feat: add skeleton with shimmer on surveyResult

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -33,9 +33,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <SurveyContext.Provider value={{ state, setState }}>
-          { state.error 
-          ? <Error error={state.error} reload={reload}/> 
-          : <SurveyListItem /> }
+          { state.error
+            ? <Error error={state.error} reload={reload}/>
+            : <SurveyListItem /> }
         </SurveyContext.Provider>
       </div>
       <Footer />
