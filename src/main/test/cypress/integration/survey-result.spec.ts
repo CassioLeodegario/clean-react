3d499ea1d45508f9bac70ecdbb99e6882c0bb335
Cassio Leodegario <cassio.leodegario@outlook.com>
2020-12-06 17:01:04 -0400

    test: ensure SurveyResult presents error if request fails

---
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -0,0 +1,24 @@
+import * as Helper from '../utils/helpers';
+import * as Http from '../utils/http-mocks';
+
+const montarTestId = (id: string): string => {
+  return `[data-testid="${id}"]`;
+};
+
+const path = /surveys/;
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET');
+
+describe('SurveyResult', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account', account);
+    });
+  });
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError();
+    cy.visit('/surveys/any_id');
+    cy.get(montarTestId('error')).should('contain.text', 'Erro inesperado. Tente novamente mais tarde');
+  });
+});
