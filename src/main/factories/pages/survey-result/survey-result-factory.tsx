c17dfab97f399ec3d4a0c09c4cf934d8ab63aaa2
cledegario <cassio.leodegario@gmail.com>
2020-12-15 16:37:03 -0400

    feat: ensure SurveyResult calls SaveSurveyResult on non active answer click

--- src/main/factories/pages/survey-result/survey-result-factory.tsx
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { SurveyResult } from '@/presentation/pages';
 import { makeRemoteLoadSurveyResult } from '../../usecases/load-survey-result/remote-load-survey-result-factory';
 import { useParams } from 'react-router-dom';
+import { makeRemoteSaveSurveyResult } from '../../usecases/save-survey-result/remote-save-survey-result-factory';
 
 export const makeSurveyResult: React.FC = () => {
   const { id } = useParams<{ id: string}>();
   return (
     <SurveyResult
       loadSurveyResult={makeRemoteLoadSurveyResult(id)}
+      saveSurveyResult={makeRemoteSaveSurveyResult(id)}
     />
   );
 };
