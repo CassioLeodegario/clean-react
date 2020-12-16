ad64fedc3971fb4840f48359665cfdf28a3ee363
cledegario <cassio.leodegario@gmail.com>
2020-12-16 09:24:20 -0400

    test: ensure SurveyResult result present new result on answer click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -6,7 +6,7 @@ const montarTestId = (id: string): string => {
 };
 
 const path = /surveys/;
-const mockLoadSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result');
+const mockLoadSuccess = (): void => Http.mockOk(path, 'GET', 'fx:load-survey-result');
 
 describe('SurveyResult', () => {
   describe('load', () => {
@@ -73,6 +73,7 @@ describe('SurveyResult', () => {
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT');
     const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT');
+    const mockSaveSuccess = (): void => Http.mockOk(path, 'PUT', 'fx:save-survey-result');
 
     beforeEach(() => {
       cy.server();
@@ -93,6 +94,26 @@ describe('SurveyResult', () => {
       mockAccessDeniedError();
       cy.get('li:nth-child(2)').click();
       Helper.testUrl('/login');
+    });
+
+    it('Should present survey result', () => {
+      mockSaveSuccess();
+      cy.get('li:nth-child(2)').click();
+      cy.get(montarTestId('question')).should('have.text', 'Other Question');
+      cy.get(montarTestId('day')).should('have.text', '23');
+      cy.get(montarTestId('month')).should('have.text', 'mar');
+      cy.get(montarTestId('year')).should('have.text', '2020');
+
+      cy.get('li:nth-child(1)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer');
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%');
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'other_image');
+      });
+      cy.get('li:nth-child(2)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer_2');
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%');
+        assert.notExists(li.find('[data-testid="image"]'));
+      });
     });
   });
 });
